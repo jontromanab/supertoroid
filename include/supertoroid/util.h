1a6058821a315e8db71ea0092ea8ac9d9024283a
root <jontromanab.abhijit@gmail.com>
2017-12-07 23:44:03 -0700

    fitting functions_1

--- include/supertoroid/util.h
+++ include/supertoroid/util.h
@@ -6,7 +6,7 @@
 #include<pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl/common/transforms.h>
-
+#include<supertoroid/st.h>
 typedef pcl::PointXYZRGB PointT;
 
 void pose_to_transform(const geometry_msgs::Pose& pose, Eigen::Affine3f& transform){
@@ -17,7 +17,7 @@ void pose_to_transform(const geometry_msgs::Pose& pose, Eigen::Affine3f& transfo
   transform.rotate(q);
 }
 
-void create_rotation_matrix(const double rx, const double ry, const double rz, Eigen::Affine3d &rot_matrix){
+void create_rotation_matrix(const double ax, const double ay, const double az, Eigen::Affine3d &rot_matrix){
   Eigen::Affine3d rx = Eigen::Affine3d(Eigen::AngleAxisd(ax, Eigen::Vector3d(1,0,0)));
   Eigen::Affine3d ry = Eigen::Affine3d(Eigen::AngleAxisd(ay, Eigen::Vector3d(0,1,0)));
   Eigen::Affine3d rz = Eigen::Affine3d(Eigen::AngleAxisd(az, Eigen::Vector3d(0,0,1)));
@@ -31,6 +31,48 @@ void create_transformation_matrix(const double tx, const double ty, const double
   create_rotation_matrix(rx, ry, rz, rot_matrix);
   Eigen::Affine3d translation_matrix(Eigen::Translation3d(Eigen::Vector3d(tx, ty, tz)));
   trans_mat = translation_matrix * rot_matrix;
+}
+
+
+void st_clampParameters(double& e1_clamped, double& e2_clamped)
+{
+  if(e1_clamped < 0.1)
+    e1_clamped = 0.1;
+  else if(e1_clamped > 1.9)
+    e1_clamped = 1.9;
+  if(e2_clamped < 0.1)
+    e2_clamped = 0.1;
+  else if(e2_clamped > 1.9)
+    e2_clamped = 1.9;
+}
+
+double st_function(const PointT& point, const supertoroid::st& param)
+{
+  double e1_clamped = param.e1;
+  double e2_clamped = param.e2;
+  st_clampParameters(e1_clamped, e2_clamped);
+  double t1 = pow(std::abs(point.x / param.a1), 2.0/e2_clamped);
+  double t2 = pow(std::abs(point.y / param.a2), 2.0/e2_clamped);
+  double t3 = pow(std::abs(point.z / param.a3), 2.0/e1_clamped);
+  double f = (pow(std::abs(t1+t2), e2_clamped/e1_clamped) - param.a4) + t3;
+  return (pow(f,e1_clamped) - 1.0 );
+}
+
+double st_function(const double &x, const double &y, const double &z, const double &a, const double &b, const double &c, const double &d, const double &e1,
+                   const double &e2)
+{
+  double e1_clamped = e1;
+  double e2_clamped = e2;
+  st_clampParameters(e1_clamped, e2_clamped);
+
+  //std::cout<<"e1: "<<e1_clamped<<" e2: "<<e2_clamped<<std::endl;
+  double t1 = pow(std::abs(x / a), 2.0/e2_clamped);
+  double t2 = pow(std::abs(y / b), 2.0/e2_clamped);
+  double t3 = pow(std::abs(z / c), 2.0/e1_clamped);
+  double f = (pow(std::abs(t1+t2), e2_clamped/e1_clamped)  - d)+ t3;
+  double value = (pow(f, e1_clamped/2.0) - 1.0) * pow(a * b * c, 0.25);
+  return (value);
+
 }
 
 #endif // UTIL_H
