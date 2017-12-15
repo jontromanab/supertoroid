ccc8b49b52cb519bd7263dafd7f738e2ad79870e
root <jontromanab.abhijit@gmail.com>
2017-12-14 22:32:22 -0700

    openni_grabber

---
+++ src/test/st_segmentation_test_pcd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<supertoroid/st_segmentation.h>
+
+#include<pcl/io/pcd_io.h>
+#include<pcl/point_types.h>
+
+int main(int argc, char * argv[])
+{
+  if(argc!=2)
+    return 0;
+
+  return 0;
+}
